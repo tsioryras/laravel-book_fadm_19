43c107f2dc42f50a785cc4efb3c465a722684e07
tsioryras7 <trasamoely@media-sante.com>
2020-04-06 15:38:09 +0200

    initialize project

---
+++ database/migrations/2020_04_06_105045_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id'); // clé primaire
+            $table->string('title', 100); // VARCHAR 100
+            $table->text('description')->nullable(); // TEXT NULL
+            $table->dateTime('published_at'); // DATETIME
+            $table->timestamps(); // timestamps
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
