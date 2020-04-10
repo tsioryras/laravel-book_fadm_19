26abf4560cf2015fd87cf446f7905a392bbf8383
tsioryras <tsioryras@users.noreply.github.com>
2020-04-10 19:18:30 +0200

    edit css and routeServiceProvider

--- resources/views/authors/index.blade.php
+++ resources/views/authors/index.blade.php
@@ -2,11 +2,13 @@
 
 @section('content')
     <h1>Authors list</h1>
-    <div class="links">
-        @forelse($authors as $author)
-            <a href="{{url('/author',['id'=>$author->id])}}" class="btn btn-outline-dark">{{$author->name}}</a>
-        @empty
-            <h1>Aucun auteur à ce jour </h1>
-        @endforelse
+    <div class="row">
+        <div class="links">
+            @forelse($authors as $author)
+                <a href="{{url('/author',['id'=>$author->id])}}" class="btn btn-outline-dark">{{$author->name}}</a>
+            @empty
+                <h1>Aucun auteur à ce jour </h1>
+            @endforelse
+        </div>
     </div>
 @endsection
