e005095973261eca885d43e593b7d1025f51ef00
tsiory Rasamoely <trasamoely@media-sante.com>
2020-05-04 09:34:32 +0200

    add browserSync in webpack and install npm / bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,6 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     @yield('css')
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
